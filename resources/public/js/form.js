802aa5d6fcb713bac721cb4bcf1a68de91fe3698
Victor Fernandes Montefusco <victor.montefusco1@gmail.com>
2020-01-21 20:53:45 -0300

    Creating generic functions to find elements; Adding callback function call to onreadystatechange; Adding response to profile creation; Functions to add and get cookies; Checking if cookie exists;

--- resources/public/js/form.js
+++ resources/public/js/form.js
@@ -1,13 +1,17 @@
 'use strict';
 
 const baseUrl = 'http://localhost:8001/api/';
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
+let profileId = 0;
 
 const httpRequest = (endpoint, options, callback) => {
     const Http = new XMLHttpRequest();
     const url = baseUrl + endpoint;
 
     Http.onreadystatechange = (e) => {
-        console.log(e);
+        if (e.target.readyState == 4)
+            callback(e.target);
     };
 
     Http.open(options.method, url);
@@ -18,22 +22,71 @@ const httpRequest = (endpoint, options, callback) => {
     Http.send(JSON.stringify(options.body));
 };
 
-const getValueFromInput = (inputName) => (document.querySelector(inputName).value)
+const getValueFromInput = (inputName) => ($(inputName).value)
 
-document.querySelector("#create-profile").addEventListener('click', () => {
+$("#create-profile").addEventListener('click', () => {
     let obj = {
-        name: getValueFromInput("#name")
+        name: getValueFromInput("#name-input")
     };
 
-    httpRequest("profile/add", { body: obj, contentType: "application/json", method: "POST" });
+    const handleProfileCreation = (response) => {
+        const responseData = JSON.parse(response.responseText);
+        const addedProfile = responseData[responseData.length - 1];
+
+        $("#name-text").innerText = addedProfile.name;
+        setCookie('profileId', addedProfile.id);
+        profileId = addedProfile.id;
+    };
+
+    const options = {
+        body: obj,
+        contentType: "application/json",
+        method: "POST"
+    };
+
+    httpRequest("profile/add", options, handleProfileCreation);
 });
 
-document.querySelectorAll('input').forEach((x) => {
+$$('input').forEach((x) => {
     x.addEventListener('focusout', () => {
-        httpRequest("todo/1/all", { method: "GET" });
+        //httpRequest("todo/1/all", { method: "GET" });
     });
 });
 
-document.querySelector('.add-task').addEventListener('click', () =>
+$('.add-task').addEventListener('click', () =>
     console.log('add')
 );
+
+const setCookie = (name, value, expiration) => {
+    document.cookie += name + '=' + value + ';';
+}
+
+const getCookie = (name) => {
+    const cookies = document.cookie.split(';');
+
+    for (let i = 0; i < cookies.length; i++) {
+        const keyValue = cookies[i].split('=');
+
+        if (keyValue[0] === name) {
+            return keyValue[1];
+        }
+    }
+}
+
+profileId = parseInt(getCookie('profileId'));
+
+if (!isNaN(profileId)) {
+    $("#name-input").style.display = 'none';
+    $("#create-profile").style.display = 'none';
+
+    const options = {
+        method: 'GET'
+    }
+
+    const handleLoggedIn = (response) => {
+        const profile = JSON.parse(response.responseText);
+        $("#name-text").innerText = profile.name;
+    };
+
+    httpRequest('profile/' + profileId, options, handleLoggedIn);
+}
